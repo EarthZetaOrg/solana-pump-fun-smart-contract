21e38831eb4e9041fb204ba8055ed1f19fd37969
earthzeta <johncriswick25@gmail.com>
2025-02-12 11:40:01 +0900

    feat: add consts, calc

--- programs/bonding_curve/src/consts.rs
+++ programs/bonding_curve/src/consts.rs
@@ -2,3 +2,6 @@ pub const INITIAL_PRICE_DIVIDER: u64 = 800_000;       // lamports per one token 
 pub const INITIAL_LAMPORTS_FOR_POOL: u64 = 10_000_000;   // 0.01SOL
 pub const TOKEN_SELL_LIMIT_PERCENT: u64 = 8000;     //  80%
 pub const PROPORTION: u64 = 1280;      //  800M token is sold on 500SOL ===> (500 * 2 / 800) = 1.25 ===> 800 : 1.25 = 640 ====> 640 * 2 = 1280
+pub const FEE: u64 = 100;      //  1%
+pub const FEE_DIVIDER: u64 = 10000;      //  100%
+pub const PRICE_DIVIDER: u64 = 100000000;      //  100% (without decimal)
--- programs/bonding_curve/src/utils/calc.rs
+++ programs/bonding_curve/src/utils/calc.rs
@@ -7,3 +7,13 @@ pub fn convert_to_float(value: u64, decimals: u8) -> f64 {
 pub fn convert_from_float(value: f64, decimals: u8) -> u64 {
     value.mul(f64::powf(10.0, decimals as f64)) as u64
 }
+
+pub fn calculate_price(price: u64, fee: u64) -> u64 {
+    price.mul(fee).div(100)
+}
+
+pub fn calculate_price_with_fee(price: u64, fee: u64) -> u64 {
+    price.mul(fee).div(100)
+}
+
+
